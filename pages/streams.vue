7a69bd3828dc7930da37868f89bf2011c40ab5a3
ArtyBob <nova1hazard@gmail.com>
2021-12-24 16:34:17 +0700

    titch api

--- pages/streams.vue
+++ pages/streams.vue
@@ -1,57 +1,66 @@
 <template>
   <div>
     <h1>Streams</h1>
-    <v-row>
-      <v-col cols="12" md="3" v-for="stream in streamsData">
-        <v-item>
-          <v-card
-            class="mx-auto"
-            max-width="344"
-            max-height="440px"
-            style="overflow: hidden"
-          >
-            <v-card-title class="text-overline mb-4">
-              <h3>{{stream.user_name}}</h3> <small>[{{stream.stream_user_id}}]</small>
-              <v-spacer></v-spacer>
-              <v-chip
-                small
-                class="green lighten-2"
-              >
-                {{stream.started_at}}
-              </v-chip>
-            </v-card-title>
-            <v-img
-              :src="stream.thumbnail_url"
-              height="200px"
-            ></v-img>
+    <div v-if="streamsData" class="streamsContainer" v-for="streamService in streamsData">
+      <div class="mb-5">
+        <v-img :src="streamService.logo"
+               class="ma-auto"
+               max-height="150"
+               max-width="250"></v-img>
+      </div>
+      <v-row>
+        <v-col cols="12" md="3" v-for="stream in streamService.data">
+          <v-item>
+            <v-card
+              class="mx-auto"
+              max-width="344"
+              max-height="440px"
+              style="overflow: hidden"
+            >
+              <v-card-title class="text-overline mb-4">
+                <h3>{{ stream.user_name }}</h3> <small>[{{ stream.stream_user_id }}]</small>
+                <v-spacer></v-spacer>
+                <v-chip
+                  small
+                  class="green lighten-2"
+                >
+                  {{ stream.started_at }}
+                </v-chip>
+              </v-card-title>
+              <v-img
+                :src="stream.thumbnail_url"
+                height="200px"
+              ></v-img>
 
-            <v-card-title>
-              {{stream.game_name}}
-              <v-chip
-                class="ma-2"
-                color="indigo"
-                text-color="white"
-              >
-                <v-avatar left>
-                  <v-icon>mdi-account-circle</v-icon>
-                </v-avatar>
-                {{stream.viewer_count}}
-              </v-chip>
-            </v-card-title>
+              <v-card-title>
+                {{ stream.game_name }}
+                <v-chip
+                  class="ma-2"
+                  color="indigo"
+                  text-color="white"
+                >
+                  <v-avatar left>
+                    <v-icon>mdi-account-circle</v-icon>
+                  </v-avatar>
+                  {{ stream.viewer_count }}
+                </v-chip>
+<!--                <small>{{ streamService.name }}</small>-->
+              </v-card-title>
 
-            <v-card-subtitle>
-              {{stream.title.substring(0,70)+".."}}
-            </v-card-subtitle>
+              <v-card-subtitle>
+                {{ stream.title.substring(0, 70) + ".." }}
+              </v-card-subtitle>
 
-            <!--        <v-card-text>-->
-            <!--          -->
-            <!--        </v-card-text>-->
-          </v-card>
-        </v-item>
-      </v-col>
-    </v-row>
+              <!--        <v-card-text>-->
+              <!--          -->
+              <!--        </v-card-text>-->
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </div>
 
-    <v-btn @click="getStreamsData()">Renew</v-btn>
+<!--    <v-btn @click="getStreamsData()">Renew</v-btn>-->
   </div>
 </template>
 
